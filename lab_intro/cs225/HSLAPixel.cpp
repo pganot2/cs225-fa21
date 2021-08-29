ad133c4b7f98399a60558bff519a199e98fe995d
Pj Ganotisi <pganot2@linux-ssh-02.ews.illinois.edu>
2021-08-28 22:38:18 -0500

    Complected HSLAPixel Class and passed all tests in tests-part1

--- lab_intro/cs225/HSLAPixel.cpp
+++ lab_intro/cs225/HSLAPixel.cpp
@@ -12,4 +12,20 @@
 using namespace std;
 
 namespace cs225 {
+    HSLAPixel::HSLAPixel() {
+        l = 1.0;
+        a = 1.0;
+    };
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1.0;
+    };
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    };
 }
--- lab_intro/cs225/HSLAPixel.h
+++ lab_intro/cs225/HSLAPixel.h
@@ -11,4 +11,14 @@
 #include <sstream>
 
 namespace cs225 {
+    class HSLAPixel {
+        public: 
+        double h;
+        double s;
+        double l;
+        double a;
+        HSLAPixel();
+        HSLAPixel(double hue, double saturation, double luminance);
+        HSLAPixel(double hue, double saturation, double luminance, double alpha);
+    };
 }
