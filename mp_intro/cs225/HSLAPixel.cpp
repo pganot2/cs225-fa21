d9a09216e4fc65f7b5afb9d18437bd48ec3aecdb
Pj Ganotisi <pganot2@linux-ssh-06.ews.illinois.edu>
2021-08-30 03:52:09 -0500

    Copied and pasted HSLAPixel.cpp and HSLAPixel.h from lab_intro

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -8,4 +8,20 @@
 #include "HSLAPixel.h"
 
 namespace cs225 {
+    HSLAPixel::HSLAPixel() {
+        l = 1.0;
+        a = 1.0;
+    };
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1.0;
+    };
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    };
 }
--- mp_intro/cs225/HSLAPixel.h
+++ mp_intro/cs225/HSLAPixel.h
@@ -7,4 +7,14 @@
 #pragma once
 
 namespace cs225 {
+    class HSLAPixel {
+        public: 
+        double h;
+        double s;
+        double l;
+        double a;
+        HSLAPixel();
+        HSLAPixel(double hue, double saturation, double luminance);
+        HSLAPixel(double hue, double saturation, double luminance, double alpha);
+    };
 }
