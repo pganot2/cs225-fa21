7273df6e1995b8d07240729fb834b97b1cb6ddb9
Pj Ganotisi <pganot2@linux-ssh-03.ews.illinois.edu>
2021-09-01 03:07:46 -0500

    Completed intro.cpp's rotate function | Passed all tests

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -5,7 +5,20 @@
 
 
 void rotate(std::string inputFile, std::string outputFile) {
+  cs225::PNG alma;
+  alma.readFromFile(inputFile);
+  //Rotate the alma png (pixel starts (x,y)) ends (width - x, height - y)
   // TODO: Part 2
+  unsigned int width = alma.width();
+  unsigned int height = alma.height();
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height / 2; y++) {
+      cs225::HSLAPixel & pixel = alma.getPixel(x, y);
+      cs225::HSLAPixel & oppPixel = alma.getPixel(width - x - 1, height - y - 1);
+      std::swap(pixel, oppPixel);
+    }
+  }
+  alma.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
