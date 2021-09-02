6482fea822e87256ba2b421fe5fd24c107f43369
Pj Ganotisi <pganot2@linux-ssh-01.ews.illinois.edu>
2021-09-01 23:10:10 -0500

    Completed all test cases. mp_intro complete

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -3,27 +3,42 @@
 
 #include <string>
 
-
 void rotate(std::string inputFile, std::string outputFile) {
-  cs225::PNG alma;
-  alma.readFromFile(inputFile);
+  cs225::PNG png;
+  png.readFromFile(inputFile);
   //Rotate the alma png (pixel starts (x,y)) ends (width - x, height - y)
   // TODO: Part 2
-  unsigned int width = alma.width();
-  unsigned int height = alma.height();
+  unsigned int width = png.width();
+  unsigned int height = png.height();
+  if (height % 2 != 0) {
+    for (unsigned x = 0; x < width/2; x++) {
+        cs225::HSLAPixel & Xposition = png.getPixel(x, height / 2);
+        cs225::HSLAPixel & oppXposition = png.getPixel(width - x - 1, height / 2);
+        std::swap(Xposition, oppXposition);
+     }
+  }
   for (unsigned x = 0; x < width; x++) {
     for (unsigned y = 0; y < height / 2; y++) {
-      cs225::HSLAPixel & pixel = alma.getPixel(x, y);
-      cs225::HSLAPixel & oppPixel = alma.getPixel(width - x - 1, height - y - 1);
+      cs225::HSLAPixel & pixel = png.getPixel(x, y);
+      cs225::HSLAPixel & oppPixel = png.getPixel(width - x - 1, height - y - 1);
       std::swap(pixel, oppPixel);
+      }
     }
-  }
-  alma.writeToFile(outputFile);
+  png.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
 
+  for (unsigned x = 0; x < width - 1; x++) {
+    for (unsigned y = 0; y < height - 1; y++) {
+      cs225::HSLAPixel & pixel = png.getPixel(x, y);
+      pixel.h = rand() % 359 + 1;
+      pixel.s = 1;
+      pixel.l = 0.5;
+      pixel.a = 1.0;
+    }
+  }
   return png;
 }
