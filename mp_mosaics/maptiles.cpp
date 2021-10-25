ccaa0cb70065b99034d2e4c1e7452b7d6f05bbdf
Pj Ganotisi <pganot2@siebl-0224-20.ews.illinois.edu>
2021-10-24 21:16:14 -0500

    Completed mp_mosaics: Monstrous Mosaics part 2 | Still need to compose mymosaic.png

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -21,7 +21,47 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
+    // It creates a new dynamically allocated MosaicCanvas
+    //, with the same number of rows and columns as the SourceImage,
+    // and returns a pointer to this object.
+    MosaicCanvas* canvas = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    
+    // Vector of points to be used to initialize kd_tree
+    vector<Point<3>> points;
+    // Map to assign a point to a specific Tile Image
+    map<Point<3>, TileImage*> map;
 
-    return NULL;
+    for (auto it = theTiles.begin(); it != theTiles.end(); it++) {
+        // Get average color of the region in SourceImage and mapTiles();
+        LUVAPixel pixel = it->getAverageColor();
+
+        Point<3> point = convertToXYZ(pixel);
+
+        points.push_back(point);
+
+        map[point] = &*it;
+    }
+
+    // KDTree to findNearestNeighbor of query point(LUVAPixel converted to Point<3>)
+    KDTree<3> tree(points);
+
+    // Loop the same size as SoureImage to place that TileImage into the MosaicCanvas 
+    // in the same tile position.
+    for (int i = 0; i < theSource.getRows(); i++) {
+        for (int j = 0; j < theSource.getColumns(); j++) {
+
+            LUVAPixel sourceImagePixel = theSource.getRegionColor(i, j);
+
+            Point<3> query = convertToXYZ(sourceImagePixel);
+
+            Point<3> nearest = tree.findNearestNeighbor(query);
+            // Gets best possible tile in map using the nearest pixel
+            TileImage* best_tile = map[nearest];
+
+            // Places that TileImage onto MosaicCanvas
+            canvas->setTile(i, j, best_tile);
+        }
+    }
+    return canvas;
 }
 
