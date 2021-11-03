9127eb85ce052ca251b767d2efa505e10efc9593
Pj Ganotisi <pganot2@siebl-0224-11.ews.illinois.edu>
2021-11-03 00:12:17 -0500

    Completed mp_mazes: Madenning Mazes part 1

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,58 @@
 /* Your code here! */
+/**
+ * @file dsets.cpp
+ * Implementation of Disjoint Sets data structure class.
+ */
+#include "dsets.h"
+ 
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void DisjointSets::addelements(int num) {
+    // Creates num elements with representative number -1 and index is the value
+    for (int i = 0; i < num; i++) {
+        uptree.push_back(-1);
+    }
+}
+
+int DisjointSets::find(int elem) {
+    if (uptree[elem] < 0) {
+        return elem;
+    } 
+    // Path Compression
+    else {
+        // return find(uptree[elem]);
+        // On Recurse back it compresses path by pointing current elem to root
+        int root = find(uptree[elem]);  
+        uptree[elem] = root;
+        return root;
+    }
+}
+
+void DisjointSets::setunion(int a, int b) {
+    int root1 = find(a);
+    int root2 = find(b);
+    int newSize = uptree[root1] + uptree[root2];
+
+    // If uptree[a]] is less than (more negative), it is the larger set;
+    // we union the smaller set, b, with a. (a is value in the set)
+    if (uptree[root1] < uptree[root2]) {
+        uptree[root2] = root1;
+        uptree[root1] = newSize;
+    } 
+    // Otherwise, do the opposite:
+    else {
+        uptree[root1] = root2;
+        uptree[root2] = newSize;
+    }
+}
+
+int DisjointSets::size(int elem) {
+    return abs(uptree[find(elem)]);
+}
+
+
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,46 @@
 /* Your code here! */
+/**
+ * @file dsets.h
+ * Implementation of Disjoint Sets data structure class.
+ *
+ * @author PJ Ganotisi
+ */
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class DisjointSets {
+    public:
+    /**
+     * @param num
+     * 
+     */
+    void addelements(int num);
+
+    /**
+     * @param num
+     * 
+     */
+    int find(int elem);
+
+    /**
+     * @param a First root to be unioned
+     * @param b Second root to be unioned
+     * 
+     */
+    void setunion(int a, int b);
+
+    /**
+     * @param num
+     * 
+     */
+    int size(int elem);
+
+    private:
+    /** Vector of ints to hold the disjoint set uptree */
+    vector<int> uptree;
+};
